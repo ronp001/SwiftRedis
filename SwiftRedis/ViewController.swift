08669fa69aaa82d7e95b44d842ad5b8e4dee6fa4
Ron Perry <ronp001@users.noreply.github.com>
2015-11-09 01:18:50 -0500

    Initial Commit

---
+++ SwiftRedis/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftRedis
+//
+//  Created by Ron Perry on 11/9/15.
+//  Copyright © 2015 Ron Perry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
