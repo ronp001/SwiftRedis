a091cd5ea917d5f1fd18e36d74c91fe87b6d51c8
Ron Perry <ronp001@users.noreply.github.com>
2015-11-09 21:58:36 -0500

    deleted ConnectionParams.swift

--- SwiftRedisTests/ConnectionParams.swift
+++ SwiftRedisTests/ConnectionParams.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 class ConnectionParams {
-    static let serverAddress = "localhost"
+    static let serverAddress = "localhost1"
     static let serverPort = UInt32(6379)
     static let auth = "connection-secret"  // warning:  including the authentication secret as an unecrypted string in an iOS app is extremely unsecure:  hackers can easily grab this string from the app.  Use this method only for the purpose of Unit Testing or if the app is distributed only to trusted devices.
 }
