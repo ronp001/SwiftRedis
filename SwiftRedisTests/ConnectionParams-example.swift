075e97593fd75497c4a7ca30b872444f9ca7b8fe
Ron Perry <ronp001@users.noreply.github.com>
2015-11-09 22:27:34 -0500

    minor documentation changes

--- SwiftRedisTests/ConnectionParams-example.swift
+++ SwiftRedisTests/ConnectionParams-example.swift
@@ -8,17 +8,17 @@
 
 import Foundation
 
-// NOTE:  This file only serves as a template, and is not referenced by the unite tests.
+// NOTE:  This file only serves as a template, and is not referenced by the unit tests.
 //
 // To run the unit tests:
 //
 // 1. Rename the class ConnectionParamsExample to ConnectionParams
 // 2. Save a copy of this file as ../../ConnectionParams.swift
-// 3. Put the real connection parameters in ConnectionParams.swift
+// 3. Put the real connection parameters (those that you don't want to share publicly) in ConnectionParams.swift
 // 4. Run the unit tests
 //
 class ConnectionParamsExample {
     static let serverAddress = "localhost"
     static let serverPort = UInt32(6379)
-    static let auth = "connection-secret"  // warning:  including the authentication secret as an unecrypted string in an iOS app is extremely unsecure:  hackers can easily grab this string from the app.  Use this method only for the purpose of Unit Testing or if the app is distributed only to trusted devices.
+    static let auth = "connection-secret"  // warning:  including the authentication secret as an unecrypted string in an iOS app is extremely unsecure:  hackers can easily grab this string from the app.  Use this method only for unit testing, or if the app is installed only on trusted devices.
 }
